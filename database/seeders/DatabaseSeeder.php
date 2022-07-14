e56bee61416cd80b053375ac9874aa601577d6a5
Luane Testa <luane.testa@pmbg.local>
2022-07-14 16:41:19 -0300

    teste seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,20 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(
+            NotaSeeder::class,
+            UsuarioSeeder::class,
+        );
     }
+
+    // public function run()
+    // {
+    //     $this->call(
+    //         NotaSeeder::class,
+    //         UsuarioSeeder::class,
+    //     );
+    // }
 }
